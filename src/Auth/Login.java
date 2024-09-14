186389734e4b763f92d002c05b6b38ea46f9f6ae
Yasser <aitelghariyasser@gmail.com>
2024-09-14 18:50:19 +0100

    creating the repositories classes

--- src/Auth/Login.java
+++ src/Auth/Login.java
@@ -2,10 +2,8 @@ package Auth;
 
 import domain.Project;
 import domain.User;
-//import Repository.ConsumptionRepository;
 import config.Database;
 import domain.enums.Role;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
